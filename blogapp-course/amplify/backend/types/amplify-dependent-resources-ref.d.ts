01178c77abc07916af1b523ed395593bcf6a78ac
Fahd Mannaa <fahdkuwait@gmail.com>
2024-03-02 20:55:33 +0300

    65

---
+++ blogapp-course/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "blogappcourse": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
---
+++ blogapp-course/src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import './globals.css'; // Adjust the path based on your CSS file's location
+
+export default function Home() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  );
+}
